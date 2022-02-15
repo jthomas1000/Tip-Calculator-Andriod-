546e53211d31a9fd08c5d8021fa288cb6d75486d
Justin Thomas <56947451+jthomas1000@users.noreply.github.com>
2022-02-15 14:54:13 -0600

    Final Commit (should be)

--- app/src/main/java/com/example/lab2a/MainActivity.java
+++ app/src/main/java/com/example/lab2a/MainActivity.java
@@ -4,10 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import com.example.lab2a.databinding.ActivityMainBinding;
 import android.os.Bundle;
-import android.widget.EditText;
+
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
+
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
@@ -23,6 +23,10 @@ public class MainActivity extends AppCompatActivity {
         binding.calcButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                String r = "100";
+
+                BigDecimal rounder = new BigDecimal(r);
                 String i = String.valueOf(binding.editTip);
                 BigDecimal tipCalc = new BigDecimal(i);
                 String e = String.valueOf(binding.editTotalbill);
@@ -40,6 +44,11 @@ public class MainActivity extends AppCompatActivity {
                     binding.outputField.setText("");
                }
                else{
+                   BigDecimal tipTotal = billCalc.multiply(tipCalc.divide(rounder));
+                   BigDecimal billTotal = billCalc.add(tipTotal);
+                   BigDecimal finalTotal = billTotal.divide(peopleCalc);
+
+                   binding.outputField.setText(String.valueOf(finalTotal));
 
                }
             }
