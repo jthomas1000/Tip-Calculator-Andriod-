89ba573b8ae4336529bd7695b9d2f1635261b217
Justin Thomas <56947451+jthomas1000@users.noreply.github.com>
2022-02-15 13:40:26 -0600

    Update MainActivity.java

--- app/src/main/java/com/example/lab2a/MainActivity.java
+++ app/src/main/java/com/example/lab2a/MainActivity.java
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import com.example.lab2a.databinding.ActivityMainBinding;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +19,31 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        binding.calcButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String i = String.valueOf(binding.editTip);
+                BigDecimal tipCalc = new BigDecimal(i);
+                String e = String.valueOf(binding.editTotalbill);
+                BigDecimal billCalc = new BigDecimal(e);
+                String o = String.valueOf(binding.editPeople);
+                BigDecimal peopleCalc = new BigDecimal(o);
+
+               if ( i == "" || i == "0" ){
+                   binding.outputField.setText("");
+               }
+               else if ( e == "" || e == "0" ){
+                    binding.outputField.setText("");
+               }
+               else if ( o == "" || o == "0" ){
+                    binding.outputField.setText("");
+               }
+               else{
+
+               }
+            }
+        });
+
     }
 }
